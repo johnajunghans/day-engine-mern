559ddf9d8d2ca7145926f1e42eea873ec37f7581
Tony Junghans <johnajunghans@gmail.com>
2023-08-05 21:25:21 -0700

    added mongoose and connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 require('dotenv').config()
 const express = require('express');
 const routineRoutes = require('./routes/routines.js');
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
@@ -13,9 +14,18 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.use('./api/routines', routineRoutes);
+app.use('/api/routines', routineRoutes);
 
-// listening
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listening
+        console.log('connected to db')
+        app.listen(process.env.PORT, () => {
+            console.log('listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
