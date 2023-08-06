0d8dad94829e0a2c373fa27ba6f9f7e636dcec55
Tony Junghans <johnajunghans@gmail.com>
2023-08-05 21:03:48 -0700

    added routines folder and started to create routing

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
+// require
 require('dotenv').config()
-
 const express = require('express');
+const routineRoutes = require('./routes/routines.js');
 
+// express app
 const app = express();
 
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+// routes
+app.use('./api/routines', routineRoutes);
+
+// listening
 app.listen(process.env.PORT, () => {
     console.log('listening on port', process.env.PORT)
 })
